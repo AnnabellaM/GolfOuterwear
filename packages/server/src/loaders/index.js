3dc666c3124152e3456e54f97cd868f1f55be202
Max Lai <m020203@gms.tku.edu.tw>
2021-10-21 15:04:54 -0500

    feat: setup mongo connection

--- packages/server/src/loaders/index.js
+++ packages/server/src/loaders/index.js
@@ -1,5 +1,10 @@
 const expressLoader = require('./express')
+const mongooseLoader = require('./mongoose')
 
 module.exports = async ({ expressApp }) => {
+  // mongo initialize
+  await mongooseLoader()
+
+  // express initialize
   expressLoader({ app: expressApp })
 }
