f8e08e25a4d85e99d4725e8699e9e11db5d78073
Max Lai <m020203@gms.tku.edu.tw>
2021-10-24 15:24:05 -0500

    feat: used cors in server

--- packages/server/src/loaders/express.js
+++ packages/server/src/loaders/express.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const api = require('../api');
+const cors = require('cors');
 
 module.exports = ({ app }) => {
+  // cors
+  app.use(cors())
+
   // body parser
   app.use(express.json());
 
